bfdcbc68e8d45d359b456955d52d4b71d995f9be
Andrey Kofanov <a14449551@gmail.com>
2025-07-05 18:00:26 +0300

    Минимальная реализация майна и сервера

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,9 +1,42 @@
 package main
 
-import "github.com/Kofandr/API_Proxy.git/internal/logger"
+import (
+	"context"
+	"github.com/Kofandr/API_Proxy.git/internal/logger"
+	"github.com/Kofandr/API_Proxy.git/internal/server"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
 
 	log := logger.New("INFO")
+
+	mainServer := server.New(log)
+
+	go func() {
+		if err := mainServer.Start(); err != nil && err != http.ErrServerClosed {
+			log.Error("Server error", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	<-sig
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	log.Info("Shutting down...")
+	if err := mainServer.Shutdown(ctx); err != nil {
+		log.Error("Shutdown failed", "error", err)
+	} else {
+		log.Info("Server stopped")
+	}
 
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,30 @@
 package server
+
+import (
+	"context"
+	"github.com/labstack/echo/v4"
+	"log/slog"
+)
+
+type Server struct {
+	echo *echo.Echo
+	addr string
+	log  *slog.Logger
+}
+
+func New(log *slog.Logger) *Server {
+	serverEcho := echo.New()
+
+	return &Server{echo: serverEcho, addr: ":8080", log: log}
+
+}
+
+func (server *Server) Start() error {
+	server.log.Info("Starting server", "addr", server.addr)
+	return server.echo.Start(server.addr)
+}
+
+func (server *Server) Shutdown(ctx context.Context) error {
+	server.log.Info("Shutting down server")
+	return server.echo.Shutdown(ctx)
+}
